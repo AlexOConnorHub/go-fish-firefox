82b63887d3306aa6ef13c01b0015484f877d1502
AlexOConnorHub <alex@oconnorville.org>
2021-10-05 00:00:07 -0400

    Working sockets

--- popup/gofish.js
+++ popup/gofish.js
@@ -1,11 +1,28 @@
-var  ws = new WebSocket("ws://localhost:8080/websocket");
-ws.onopen = function() {
-    ws.send("Hello, world");
+// Leave at top, makes websocket
+var ws = new WebSocket("ws://localhost:4444/websocket");
+var keepAlive = setInterval(function() { ws.send("PING"); }, 55000);
+
+// INIT message upon connection
+ws.onopen = function () {
+    ws.send("INIT");
 };
+
+// Upon receiving a message
 ws.onmessage = function (evt) {
     console.log(evt.data);
 };
 
-var clickButton = function() {
+// Add functions here
+handler = function() {
     ws.send("Test");
 }
+
+random = function() {
+    console.log("Hello");
+}
+
+// This is where to add event listeners
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("TestButton").addEventListener("click", handler);
+});
+
